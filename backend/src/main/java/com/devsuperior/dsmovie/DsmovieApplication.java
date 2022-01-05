f3ab02789b12ff4ca96dd8b7d45049913d661441
Nelio Alves <acenelio@gmail.com>
2022-01-04 21:17:49 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieApplication.class, args);
+	}
+
+}
